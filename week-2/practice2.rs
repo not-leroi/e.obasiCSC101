a1691823e180dc4cc5b3e9413db7a2edc5dc4fdf
not-leroi <chinemeremobasi2306@gmail.com>
2024-10-26 15:30:45 +0100

    (Week 2)

---
+++ week-2/practice2.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+	println!();//prints just a
+
+	println!("hello");//prints
+
+	println!("format{} arguments","some");//prints format some arguments
+
+	println!("My name is {}. I am the classrep pf stream 2","Darago William-Jumbo");
+}
